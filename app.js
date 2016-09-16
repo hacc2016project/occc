61decc938876830f5945027d5db1a1f5bf423425
Trav <jorkilla@gmail.com>
2016-09-15 19:21:50 -1000

    test json
    surf’s up

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
-
 var serv = require("http").Server(app);
+var schedule = require('node-schedule');
+
 
 // make app use the `bodyParser()` middleware for all routes
-app.use(bodyParser());
+// app.use(bodyParser());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//scheduler
+var scheduleTest = schedule.scheduleJob('21 * * * *', function(){
+  console.log("it works");
+});
 
 app.get('/', function(req,res){
 	res.sendFile(__dirname+'/client/index.html');
@@ -21,7 +29,10 @@ app.get('/admin', function(req, res){
   res.sendFile(__dirname+'/admin/admin.html');
 });
 
-
+// json test route
+app.get('/jsonTest', function(req, res){
+  res.sendFile(__dirname+'/jsonTest/jsonTest.html');
+});
 
 // This route receives the posted form.
 // As explained above, usage of 'body-parser' means
@@ -31,10 +42,9 @@ app.post('/', function(req, res){
   var userAddress = req.body.userAddress;
 
   var html = 'Hello: ' + userName + '.<br>' +
-             'you live at' + userAddress + '<br>'+ 
+             'you live at <br>' + userAddress + '<br>'+ 
              '<a href="/">Try again.</a>';
   res.send(html);
-  
 });
 
 app.use('/client',express.static(__dirname + '/client'));
